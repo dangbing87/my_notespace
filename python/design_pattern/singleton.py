44688075b51c3b836a5f5b1fc4da592612c3c4ae
danny <1211740652@qq.com>
2016-01-17 22:11:20 +0800

    [REV]共享属性的单例，但会开辟新的内存空间

--- python/design_pattern/singleton.py
+++ python/design_pattern/singleton.py
@@ -8,3 +8,14 @@ class Singleton(object):
         if not hasattr(cls, '_instance'):
             cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
         return cls._instance
+
+
+class ShareData(object):
+    """
+    只共享数据，但每次new都会开辟内存空间
+    """
+
+    shared_state = {}
+
+    def __init__(self):
+        self.__dict__ = self.shared_state
