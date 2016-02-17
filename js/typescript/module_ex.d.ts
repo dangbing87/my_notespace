30807c17ddc67a01c8b6377230a7be2dc225c375
hexiang <1211740652@qq.ocm>
2016-02-17 11:05:18 +0800

    [REV]Learn typescript module

---
+++ js/typescript/module_ex.d.ts
@@ -0,0 +1,23 @@
+declare module "Todo" {
+    export enum TodoStatus {
+        done,
+        doing,
+        dont,
+    }
+
+    export interface TodoModel {
+        id: number;
+        item: string;
+        itemStatus: TodoStatus;
+    }
+
+    export interface TodoInterface {
+        todoList: Array<TodoModel>;
+
+        getTodo(id: number): TodoModel;
+        addTodo(todo: TodoModel): void;
+        changeStatus(id: number, itemStatus: TodoStatus): void;
+        deleteItem(id: number): void;
+        display(): void;
+    }
+}
---
+++ js/typescript/test_module1.ts
@@ -0,0 +1,45 @@
+///<reference path="module_ex.d.ts"/>
+import todo = require("Todo");
+
+class TodoCtrl implements todo.TodoInterface {
+
+    public todoList: Array<todo.TodoModel> = [];
+
+    getTodo(id: number): todo.TodoModel {
+        var todoInfo: todo.TodoModel;
+        this.todoList.forEach(todoItem => {
+            if (todoItem.id == id) {
+                todoInfo = todoItem;
+                return;
+            }
+        });
+
+        return todoInfo;
+    }
+
+    addTodo(todo: todo.TodoModel): void {
+        this.todoList.push(todo);
+    }
+
+    changeStatus(id: number, status: todo.TodoStatus): void {
+        var todoItem = this.getTodo(id);
+        todoItem.itemStatus = status;
+    }
+
+    deleteItem(id: number): void {
+        var newTodoList: Array<todo.TodoModel> = [];
+
+        this.todoList.forEach(todo=> {
+            if (todo.id != id) {
+                newTodoList.push(todo);
+            }
+        });
+    }
+
+    display(): void {
+        this.todoList.forEach(todoItem => {
+            var todoInfo = `id: ${todoItem.id}  item: ${todoItem.item}   status: ${todoItem.itemStatus}`;
+            console.log(todoInfo);
+        });
+    }
+}
