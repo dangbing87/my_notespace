758902cfcf52afa2c19312bf13010381b6888497
wskssau <1211740652@qq.com>
2016-06-21 05:44:09 +0800

    [REV]reducer todoApp

--- font_end/redux/static/js/todo/reducers.js
+++ font_end/redux/static/js/todo/reducers.js
@@ -8,6 +8,19 @@ function todoApp(state, action) {
     switch (action.type) {
         case SET_VISIBILITY_FILTER:
             return Object.assign({}, state, { visibilityFilter: action.filter });
+        case ADD_TODO:
+            return Object.assign({}, state, {
+                todos: state.todos.concat([{ text: action.text, completed: false }])
+            });
+        case COMPLETE_TODO:
+            return Object.assign({}, state, {
+                todos: state.todos.map(function (todo, index) {
+                    if (index === action.index) {
+                        return Object.assign({}, todo, { completed: !todo.completed });
+                    }
+                    return todo;
+                })
+            });
         default:
             return state;
     }
--- font_end/redux/static/ts/todo/reducers.ts
+++ font_end/redux/static/ts/todo/reducers.ts
@@ -23,6 +23,19 @@ function todoApp(state: TodoState = initialState, action: Action): TodoState {
     switch (action.type) {
         case SET_VISIBILITY_FILTER:
             return Object.assign({}, state, { visibilityFilter: action.filter });
+        case ADD_TODO:
+            return Object.assign({}, state, {
+                todos: [ ...state.todos, { text: action.text, completed: false }]
+            });
+        case COMPLETE_TODO:
+            return Object.assign({}, state, {
+                todos: state.todos.map((todo, index) => {
+                    if (index === action.index) {
+                        return Object.assign({}, todo, { completed: !todo.completed });
+                    }
+                    return todo;
+                })
+            });
         default:
             return state;
     }
