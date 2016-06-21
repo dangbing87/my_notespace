ac9e32a213fc8e85cca72a44c34d942218e8373c
hexiang <1211740652@qq.ocm>
2016-06-21 14:50:13 +0800

    [REV]Todo Reducer

--- font_end/redux/static/js/todo/reducers.js
+++ font_end/redux/static/js/todo/reducers.js
@@ -1,27 +1,35 @@
-///<reference path="actions" />
+///<reference path="actions.ts" />
+/// <reference path="../typings/redux/redux.d.ts"/>
+var combineReducers = Redux.combineReducers;
 var initialState = {
     visibilityFilter: VisibilityFilters.SHOW_ALL,
     todos: []
 };
-function todoApp(state, action) {
-    if (state === void 0) { state = initialState; }
+function todos(state, action) {
     switch (action.type) {
-        case SET_VISIBILITY_FILTER:
-            return Object.assign({}, state, { visibilityFilter: action.filter });
         case ADD_TODO:
-            return Object.assign({}, state, {
-                todos: state.todos.concat([{ text: action.text, completed: false }])
-            });
+            return state.concat([{ text: action.text, completed: false }]);
         case COMPLETE_TODO:
-            return Object.assign({}, state, {
-                todos: state.todos.map(function (todo, index) {
-                    if (index === action.index) {
-                        return Object.assign({}, todo, { completed: !todo.completed });
-                    }
-                    return todo;
-                })
+            return state.map(function (todo, index) {
+                if (index === action.index) {
+                    return Object.assign({}, todo, { completed: !todo.completed });
+                }
+                return todo;
             });
         default:
             return state;
     }
 }
+function visibilityFilter(state, action) {
+    if (state === void 0) { state = VisibilityFilters.SHOW_ALL; }
+    switch (action.type) {
+        case SET_VISIBILITY_FILTER:
+            return action.text;
+        default:
+            return state;
+    }
+}
+var todoApp = combineReducers({
+    visibilityFilter: visibilityFilter,
+    todos: todos
+});
--- font_end/redux/static/ts/todo/reducers.ts
+++ font_end/redux/static/ts/todo/reducers.ts
@@ -1,4 +1,7 @@
-///<reference path="actions" />
+///<reference path="actions.ts" />
+/// <reference path="../typings/redux/redux.d.ts"/>
+
+import combineReducers = Redux.combineReducers;
 
 interface Todo {
     text: string;
@@ -19,24 +22,33 @@ const initialState: TodoState = {
     todos: []
 };
 
-function todoApp(state: TodoState = initialState, action: Action): TodoState {
+function todos(state: Array<Todo>, action: Action): Array<Todo> {
     switch (action.type) {
-        case SET_VISIBILITY_FILTER:
-            return Object.assign({}, state, { visibilityFilter: action.filter });
         case ADD_TODO:
-            return Object.assign({}, state, {
-                todos: [ ...state.todos, { text: action.text, completed: false }]
-            });
+            return  [ ...state, { text: action.text, completed: false }]
+
         case COMPLETE_TODO:
-            return Object.assign({}, state, {
-                todos: state.todos.map((todo, index) => {
-                    if (index === action.index) {
-                        return Object.assign({}, todo, { completed: !todo.completed });
-                    }
-                    return todo;
-                })
+            return state.map(function(todo, index) {
+                if (index === action.index) {
+                    return Object.assign({}, todo, { completed: !todo.completed });
+                }
+                return todo;
             });
         default:
             return state;
     }
 }
+
+function visibilityFilter(state: string = VisibilityFilters.SHOW_ALL, action: Action): string {
+    switch (action.type) {
+        case SET_VISIBILITY_FILTER:
+            return action.text;
+        default:
+            return state;
+    }
+}
+
+const todoApp = combineReducers({
+    visibilityFilter,
+    todos
+});
