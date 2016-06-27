7a306546909213a0f59f1fea7f63b5a7e666f201
hexiang <1211740652@qq.ocm>
2016-06-27 17:42:32 +0800

    [REV]Todo Component

--- font_end/redux/static/js/todos/build/component.js
+++ font_end/redux/static/js/todos/build/component.js
@@ -1,9 +1,40 @@
+const todos = [
+    {
+        text: "test test one",
+        completed: false
+    },
+    {
+        text: "test test two",
+        completed: false
+    },
+    {
+        text: "test test three",
+        completed: false
+    },
+    {
+        text: "test test four",
+        completed: false
+    }
+];
+
 var AddTodoBoxComponent = React.createClass({displayName: "AddTodoBoxComponent",
     render: function() {
         return (
-            React.createElement("div", null,
-                React.createElement("input", {type: "text"}),
-                React.createElement("button", null, "Save")
+            React.createElement("form", {className: "am-form"}, 
+                React.createElement("fieldset", {
+                className: "am-g"}, 
+                    React.createElement("label", {
+                    className: "am-u-sm-10"
+                    }, 
+                        React.createElement("input", {
+                        type: "text"})
+                    ), 
+
+                    React.createElement("button", {
+                    className: "am-btn am-btn-primary am-u-sm-2"}, 
+                        "Save"
+                    )
+                )
             )
         );
     }
@@ -12,20 +43,76 @@ var AddTodoBoxComponent = React.createClass({displayName: "AddTodoBoxComponent",
 var TodoComponent = React.createClass({displayName: "TodoComponent",
     render: function() {
         return (
-            React.createElement("li", null, this.props.text)
+            React.createElement("li", {
+            style: {
+                textDecoration: this.props.completed ? 'line-through' : 'none',
+                cursor: this.props.completed ? 'default' : 'pointer'
+            }
+            }, 
+            this.props.text
+            )
         );
     }
 });
 
 var TodoListComponent = React.createClass({displayName: "TodoListComponent",
     render: function() {
-        var todoNodes = this.props.map(function(todo) {
-            return (React.createElement(TodoComponent, {text: todo.text}));
+        var todoNodes;
+        todoNodes = this.props.todos.map(function(todo, i) {
+            return (
+                React.createElement(TodoComponent, {
+                key: i, 
+                completed: todo.completed, 
+                text: todo.text})
+            );
         });
 
         return (
-            React.createElement("ul", null,
+            React.createElement("ul", {className: "am-list am-list-static"}, 
                 todoNodes
+            )
+        );
+    }
+});
+
+var Footer = React.createClass({displayName: "Footer",
+    renderFilter: function(filter, name) {
+        if (filter === this.props.filter) {
+            return (
+                React.createElement("button", {type: "button", 
+                className: "am-btn am-btn-secondary am-u-sm-3", 
+                disabled: "disabled"
+                }, 
+                name
+                )
+            );
+        }
+
+        return (
+            React.createElement("button", {
+            type: "button", 
+            className: "am-btn am-btn-secondary am-u-sm-3"
+            }, 
+            name
+            )
+        );
+    },
+
+    renderPlaceholder: function() {
+        return (
+            React.createElement("div", {className: "am-u-sm-1"}, " ")
+        );
+    },
+
+    render: function() {
+        return (
+            React.createElement("footer", {className: "am-g"}, 
+                this.renderFilter("SHOW_ALL", "ALl"), 
+                this.renderPlaceholder(), 
+                this.renderFilter("SHOW_COMPLETED", "Completed"), 
+                this.renderPlaceholder(), 
+                this.renderFilter("SHOW_ACTIVE", "Active"), 
+                this.renderPlaceholder()
             )
         );
     }
@@ -34,9 +121,10 @@ var TodoListComponent = React.createClass({displayName: "TodoListComponent",
 var App = React.createClass({displayName: "App",
     render: function() {
         return (
-            React.createElement("div", null,
-                React.createElement(AddTodoBoxComponent, null),
-                React.createElement(TodoListComponent, null)
+            React.createElement("div", {className: "am-u-sm-centered am-u-sm-6"}, 
+                React.createElement(AddTodoBoxComponent, null), 
+                React.createElement(TodoListComponent, {todos: todos}), 
+                React.createElement(Footer, null)
             )
         );
     }
--- font_end/redux/static/js/todos/src/component.js
+++ font_end/redux/static/js/todos/src/component.js
@@ -1,10 +1,41 @@
+const todos = [
+    {
+        text: "test test one",
+        completed: false
+    },
+    {
+        text: "test test two",
+        completed: false
+    },
+    {
+        text: "test test three",
+        completed: false
+    },
+    {
+        text: "test test four",
+        completed: false
+    }
+];
+
 var AddTodoBoxComponent = React.createClass({
     render: function() {
         return (
-            <div>
-                <input type="text" />
-                <button>Save</button>
-            </div>
+            <form className="am-form">
+                <fieldset
+                className="am-g">
+                    <label
+                    className="am-u-sm-10"
+                    >
+                        <input
+                        type="text" />
+                    </label>
+
+                    <button
+                    className="am-btn am-btn-primary am-u-sm-2">
+                        Save
+                    </button>
+                </fieldset>
+            </form>
         );
     }
 });
@@ -12,21 +43,77 @@ var AddTodoBoxComponent = React.createClass({
 var TodoComponent = React.createClass({
     render: function() {
         return (
-            <li>{this.props.text}</li>
+            <li
+            style={{
+                textDecoration: this.props.completed ? 'line-through' : 'none',
+                cursor: this.props.completed ? 'default' : 'pointer'
+            }}
+            >
+            {this.props.text}
+            </li>
         );
     }
 });
 
 var TodoListComponent = React.createClass({
     render: function() {
-        var todoNodes = this.props.map(function(todo) {
-            return (<TodoComponent text={todo.text} />);
+        var todoNodes;
+        todoNodes = this.props.todos.map(function(todo, i) {
+            return (
+                <TodoComponent
+                key={i}
+                completed={todo.completed}
+                text={todo.text} />
+            );
         });
 
         return (
-            <ul>
+            <ul className="am-list am-list-static">
                 {todoNodes}
             </ul>
+        );
+    }
+});
+
+var Footer = React.createClass({
+    renderFilter: function(filter, name) {
+        if (filter === this.props.filter) {
+            return (
+                <button type="button"
+                className="am-btn am-btn-secondary am-u-sm-3"
+                disabled="disabled"
+                >
+                {name}
+                </button>
+            );
+        }
+
+        return (
+            <button
+            type="button"
+            className="am-btn am-btn-secondary am-u-sm-3"
+            >
+            {name}
+            </button>
+        );
+    },
+
+    renderPlaceholder: function() {
+        return (
+            <div className="am-u-sm-1">&nbsp;</div>
+        );
+    },
+
+    render: function() {
+        return (
+            <footer className="am-g">
+                {this.renderFilter("SHOW_ALL", "ALl")}
+                {this.renderPlaceholder()}
+                {this.renderFilter("SHOW_COMPLETED", "Completed")}
+                {this.renderPlaceholder()}
+                {this.renderFilter("SHOW_ACTIVE", "Active")}
+                {this.renderPlaceholder()}
+            </footer>
         );
     }
 });
@@ -34,9 +121,10 @@ var TodoListComponent = React.createClass({
 var App = React.createClass({
     render: function() {
         return (
-            <div>
+            <div className="am-u-sm-centered am-u-sm-6">
                 <AddTodoBoxComponent />
-                <TodoListComponent />
+                <TodoListComponent todos={todos} />
+                <Footer />
             </div>
         );
     }
