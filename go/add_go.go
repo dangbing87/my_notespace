03cc5f2ac360b46d2cbe23e1c95367a231004ea1
hexiang <1211740652@qq.ocm>
2016-08-04 16:59:29 +0800

    [REV]How to use go

--- go/add_go.go
+++ go/add_go.go
@@ -1,18 +1,21 @@
 package main
 
 import "fmt"
-import "time"
 
-func Add(x, y int) {
-	var z int = 0
-	z = x + y
-	go fmt.Println(z)
-	time.Sleep(time.Millisecond)
+func Add(x int, c chan int) {
+    var sum int
+    sum = x + x
+    fmt.Printf("%d+%d=%d\n", x, x, sum)
+    c <- sum
 }
 
 func main() {
-	for i := 0; i < 10; i++ {
-		Add(i, i)
-		// go Add(i, i)
-	}
+    var c chan int
+    c = make(chan int)
+
+    for i := 0; i < 10; i++ {
+        go Add(i, c)
+        go Add(i*2, c)
+        _, _ = <-c, <-c
+    }
 }
