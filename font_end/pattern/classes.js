c43d5e198251a5ceb0833197795986603818dcaf
danny <1211740652@qq.com>
2016-08-18 04:53:05 +0800

    [ADD]Js Classes

---
+++ font_end/pattern/classes.js
@@ -0,0 +1,89 @@
+var __extend = function(b, d) {
+    for (var p in d) {
+        b[p] = d[p];
+    }
+};
+
+var __super = function(_object) {
+    if (_object.init) {
+        return _object.init();
+    }
+
+    return {};
+};
+
+var __mixin = function(_mixin) {
+    var self = {},
+
+        f = function(_object) {
+            if (_object.init) {
+                return _object.init();
+            }
+            return {};
+        };
+
+    if (_mixin.length > 1) {
+        for (var m in _mixin) {
+            __extend(self, f(_mixin[m]));
+        }
+    } else if (_mixin) {
+        __extend(self, f(_mixin));
+    }
+
+    return self;
+};
+
+var A = (function() {
+    function init() {
+        var self = {};
+        self.word = 'hello';
+
+        return self;
+    }
+
+    return {
+        'init': init
+    };
+}());
+
+var MixinA = (function() {
+    function init() {
+        var self = {};
+        self.word = 'hello';
+
+        return self;
+    }
+
+    return {
+        'init': init
+    };
+}());
+
+var MixinB = (function() {
+    function init() {
+        var self = {};
+        self.name = 'hello';
+
+        return self;
+    }
+
+    return {
+        'init': init
+    };
+}());
+
+var B = (function(_super, _mixin) {
+    function init() {
+        var self = _super;
+
+        console.log(_mixin);
+
+        return self;
+    }
+
+    return {
+        'init': init
+    };
+}(__super(A), __mixin([MixinA, MixinB])));
+
+var b =  new B.init();
