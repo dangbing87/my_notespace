78bd3b789b3bbd51220d8a03b079b70555ce01ef
danny <1211740652@qq.com>
2016-08-18 03:03:56 +0800

    [REV]Singletion Decorator

--- font_end/pattern/singleton.js
+++ font_end/pattern/singleton.js
@@ -1,31 +1,89 @@
+var decoratorSingleton = (function() {
+
+    var wrap = function(func) {
+        var instantiated;
+
+        function init(initArgs) {
+            return func(initArgs);
+        }
+
+        return {
+            'getInstance': function(args) {
+                if (!instantiated) {
+                    instantiated = init(args);
+                }
+
+                return instantiated;
+            }
+        };
+    };
+
+    return {
+        'creat': function(func) {
+            return wrap(func);
+        }
+    };
+}());
+
 var Person = (function() {
-    var instantiated,
-        self = {};
+    var instantiated;
 
 
     function init() {
+        var self = {};
+
+        self.say = function() {
+            console.log("hi " + self.word);
+        };
+
+        self.word = "";
+
         return self;
-    };
-
-    function getInstance() {
-        if (!instantiated) {
-            instantiated = init();
-        }
-
-        return instantiated;
     }
 
+    return {
+        'getInstance': function() {
+            if (!instantiated) {
+                instantiated = init();
+            }
+
+            return instantiated;
+        }
+    };
+}());
+
+var Person2 = decoratorSingleton.creat(function() {
+    var self = {};
+
+    self.word = 'hello';
+
     self.say = function() {
-        console.log("hi " + self.word);
+        console.log(self.word);
     };
 
-    self.word = "";
+    return self;
+});
 
-    return { "getInstance": getInstance };
-})();
+
+var Cup = decoratorSingleton.creat(function(color) {
+    var self = {};
+
+    self.color = color || 'blue';
+
+    return self;
+});
 
 var a = new Person.getInstance();
 var b = new Person.getInstance();
 
-a.word = "this is singleton";
-b.say();
+b.word = '';
+// a.say();
+
+var cup1 = Cup.getInstance('red'),
+    cup2 =Cup.getInstance('green');
+
+console.log(cup1);
+console.log(cup2);
+cup1.color = 'white';
+console.log(cup2);
+
